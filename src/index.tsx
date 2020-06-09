9b41dde10e2166310bdd513b347ae90f3719eaeb
kaoru <kaoru@birman-mac.local>
2020-06-09 23:54:26 +0900

    operation verification

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MyDocument from 'src/App';
+
+ReactDOM.render(<MyDocument />, document.querySelector('#root') as HTMLElement);
